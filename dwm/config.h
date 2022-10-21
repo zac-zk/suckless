73049c2bdc95350b38408a39c504768520f8dc8d
Juvenileone <18673905988@163.com>
2022-10-21 09:12:56 +0800

    all:change font

--- dwm/config.h
+++ dwm/config.h
@@ -14,8 +14,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;   /* 0 means no systray */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
-static const char *fonts[]               = { "JetBrainsMono Nerd Font Mono:size=15" };
-static const char dmenufont[]			 = "SauceCodePro Nerd Font Mono:size=15";
+static const char *fonts[]               = { "JetBrains Mono:size=15" };
+static const char dmenufont[]			 = "JetBrains Mono:size=15";
 static const char col_gray1[]            = "#222222";
 static const char col_gray2[]            = "#444444";
 static const char col_gray3[]            = "#bbbbbb";
