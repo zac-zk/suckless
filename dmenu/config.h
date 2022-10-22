03961973eea47fa5f8a0e3489ecbc6bb9c8f8b68
Juvenileone <18673905988@163.com>
2022-10-22 15:53:25 +0800

    dmenu:change color

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,15 +4,9 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=10"
+	"JetBrains Mono:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char normbgcolor[]           = "#2E3440";
-static const char normbordercolor[]       = "#3B4252";
-static const char normfgcolor[]           = "#ECEFF4";
-static const char selfgcolor[]            = "#D8DEE9";
-static const char selbordercolor[]        = "#5E81AC";
-static const char selbgcolor[]            = "#5E81AC";
 static char termcol0[] = "#3b4252"; /* black   */
 static char termcol1[] = "#bf616a"; /* red     */
 static char termcol2[] = "#a3be8c"; /* green   */
@@ -24,9 +18,9 @@ static char termcol7[] = "#e5e9f0"; /* white   */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { termcol7, termcol0 },
-	[SchemeSel] = { termcol5, termcol5 },
-	[SchemeSelHighlight] = { termcol1, termcol5 },
-	[SchemeNormHighlight] = { termcol1, termcol0 },
+	[SchemeSel] = { termcol7, termcol1 },
+	[SchemeSelHighlight] = { termcol3, termcol1 },
+	[SchemeNormHighlight] = { termcol3, termcol0 },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { termcol1, "#00ffff" },
 };
